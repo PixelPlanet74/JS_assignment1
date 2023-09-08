742027c399c63a9d928ac830abaec6ec05d91b6b
Pixel Planet <pixelplanet74@gmail.com>
2023-09-08 18:03:48 +0500

    JS Assignment1

---
+++ app.js
@@ -0,0 +1,113 @@
+// Chapter 1 (Alerts)
+
+// Q no:1
+alert("Muhammad Farooq");
+alert("Markatiya");
+alert("xyz@gmail.com");
+alert("03XX-XXXXXXX");
+alert("●●●●●●●●●");
+
+// Q no:2
+alert("You're learning JavaScript!");
+
+// Q no:3
+alert("Good Bye :)");
+
+// Chapter 2 (Variables for string)
+
+// Q no:1
+var camelcaseVariable = "Camel Case Variable";
+
+// Q no:2
+var graphicDesigners;
+graphicDesigners = "We are Graphic Designers";
+
+// Q no:3
+var teamName = "Team Designers";
+alert(teamName);
+
+// Q no:4
+var bestMan = "Charlie";
+bestMan = "Charlie Chaplin";
+
+// Chapter 2 (Variables for numbers)
+
+var caseQty;
+caseQty = 144;
+
+var num = "9";
+var num = 9;
+
+var sum;
+sum = 2 + 2;
+
+var productPrice = 2500;
+var productQuantity = 38;
+var prderTotal = productPrice * productQuantity;
+
+var variableNum = 3;
+variableNum = variableNum + 2;
+
+// Chapter 4 (Variable names Legal and Illegal)
+
+// Q no:1
+// var product cost = 3.45;
+var productCost = 3.45;
+
+// Q no:2
+var Nameofband;
+var NameofBand;
+
+// Q no:3
+var $legal_variable = 100;
+
+// Q no:4
+var farooqMarkatiya;
+
+// Q no:5
+// Legal Variables
+var $legal_variable;
+var Legal12variable;
+var legalVariable;
+var variable_legal;
+var variable2$legal;
+
+// Illegal Variables
+// var 12illegal_variable;
+// var illegal@variable;
+// var &illlegalVariable;
+// var illegal^var;
+// var illegal-variable;
+
+// Chapter 5 (Math Expression I)
+
+// Q no:1
+Name = "modulus operator";
+// Symbol = %;
+
+// Q no:2
+var numm = 20 % 6;
+// Ans: 2
+
+// Q no:3
+var largeNum = 1000 * 2000;
+
+// Q no:4
+var $1 = 250;
+var $2 = 100;
+var variableQ4 = $1 - $2;
+
+// Q no:5
+var $_1 = 30;
+var $_2 = 3;
+var result = $_1 % $_2;
+
+// Q no:6
+var var1 = 6;
+var var2 = 5;
+var varResult = var1 * var2;
+alert(varResult);
+
+// THE END
+
+
